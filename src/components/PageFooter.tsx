21374f25cd94adcb2e0f4ec2370b9c6950605604
Alessia B <borys.alessia@gmail.com>
2022-11-03 20:05:11 +0100

    format

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,11 +1,11 @@
 function PageFooter(): JSX.Element {
-    return (
-      <footer>
-        <br/>
-        <p>________________________________________________________________</p>
-        <h4>Apparently all beautiful places have names that start with C</h4>
-      </footer>
-    );
-  }
-  
-  export default PageFooter;
+  return (
+    <footer>
+      <br />
+      <p>________________________________________________________________</p>
+      <h4>Apparently all beautiful places have names that start with C</h4>
+    </footer>
+  );
+}
+
+export default PageFooter;
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,9 +1,9 @@
 function PageHeader(): JSX.Element {
-    return (
-      <header>
-       <h1>A few of my favourite places</h1>
-      </header>
-    );
-  }
-  
-  export default PageHeader;
+  return (
+    <header>
+      <h1>A few of my favourite places</h1>
+    </header>
+  );
+}
+
+export default PageHeader;
