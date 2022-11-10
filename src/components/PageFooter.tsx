122818578829f0f4b4f3b8837e5cd6af020a14a0
Alessia B <borys.alessia@gmail.com>
2022-11-10 15:42:04 +0100

    added link to Ben's

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -4,6 +4,13 @@ function PageFooter(): JSX.Element {
       <br />
       <p>________________________________________________________________</p>
       <h4>Apparently all beautiful places have names that start with C</h4>
+      <p>
+        Check out Ben's favourite places{" "}
+        <a href="https://636b80ea5cd45237fbec105a--tranquil-marzipan-8a68ed.netlify.app/">
+          here
+        </a>
+        !
+      </p>
     </footer>
   );
 }
